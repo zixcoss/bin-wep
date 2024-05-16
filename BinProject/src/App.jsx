e15c398d830980192330bfb7bbfd97cbcf2d3109
Nontawat Noitubtim <139257120+4mGroot@users.noreply.github.com>
2024-05-16 23:41:26 +0700

    Sidebar

--- BinProject/src/App.jsx
+++ BinProject/src/App.jsx
@@ -1,12 +1,13 @@
 
 import './App.css'
 import Sidebar from './Component/Sidebar'
-
+import Bin from './Component/Img/Bin.png'
 function App() {
   return (
     <>
       <div>
         <Sidebar/>
+        
       </div>
     </>
   )
--- BinProject/src/Component/Sidebar.jsx
+++ BinProject/src/Component/Sidebar.jsx
@@ -4,15 +4,21 @@ import Bin from './Img/Bin.png'
 function Sidebar() {
   return (
     <>
-        <div className="Container border-2 border-rose-600 w-64 h-screen bg-green-100">
-            <div className='Container-logo flex pt-2 border-2 border-green-600'>
-                <div className="border-2 border-blue-600 logo w-24 flex-grow">
+        <div className="Container border-2 border-rose-600 w-64 h-screen bg-green-200">
+            <div className='Container-logo flex pt-4 border-2 border-green-600'>
+                <div className="border-2 border-blue-600 logo w-24 flex-grow ">
                     <img src={Bin} alt="My Image" />
                 </div>
                 <div className="flex justify-center items-center text-6xl text-green-600/100 namelogo border-2 border-blue-600 w-32 flex-grow">
                     <h1 className=''>BIN</h1>
                 </div>
             </div> 
+            <div className='border-2 border-green-600 mt-6'>
+                <p>menu</p>
+                <div className=''> 
+
+                </div>
+            </div>
         </div>
     </>
     
