e2bed87b2ccd5420ca1572e78f54cf2117d359aa
Nontawat Noitubtim <139257120+4mGroot@users.noreply.github.com>
2024-05-17 17:15:27 +0700

    Sidebar final

--- BinProject/src/Component/Sidebar.jsx
+++ BinProject/src/Component/Sidebar.jsx
@@ -4,32 +4,38 @@ import home from './Img/home.png'
 import controller from './Img/game-controller.png'
 import setting from './Img/settings.png'
 
+
 function Sidebar() {
   return (
     <>
-        <div className="Container border-2 border-rose-600 w-64 h-screen bg-green-200">
-            <div className='Container-logo flex pt-4 border-2 border-green-600'>
-                <div className="border-2 border-blue-600 logo w-24 flex-grow ">
+        {/* กรอบใหญ่สุด */}
+        <div className="w-72 h-screen bg-green-200">
+            {/* กรอบของโลโก้กับชื่อ */}
+            <div className='flex pt-4 shadow-xl rounded-lg'> 
+                <div className="ml-2 logo w-24 flex-grow mb-4">
                     <img src={Bin} alt="My Image" />
                 </div>
-                <div className="flex justify-center items-center text-6xl text-green-600/100 namelogo border-2 border-blue-600 w-32 flex-grow">
-                    <h1 className=''>BIN</h1>
+                <div className="flex justify-center items-center text-6xl namelogo w-32 flex-grow">
+                    <h2 className='font-bold text-emerald-900'>BIN</h2>
                 </div>
             </div> 
-            <div className='border-2 border-green-600 mt-6 '>
-                <p className='text-3xl text-slate-400 text-center'>menu</p>
-                <div className='flex mt-6' >
-                    <img src={home} alt="" className='w-16'/>
-                    <h2>Dashboard</h2> 
-                </div>
-                <div className='flex'>
-                    <img src={controller} alt="" className='w-16'/>
-                    <h2>Controller</h2>
-                </div>
-                <div className='flex'>
-                    <img src={setting} alt="" className='w-16'/>
-                    <h2>Setting</h2>
-                </div>
+            {/* กรอบของพวก sidebar */}
+            <div className='mt-2'>
+                <p className='text-3xl text-slate-400 mt-4'>menu</p>
+                <ul>
+                    <li className="flex items-center mt-2 py-4 shadow-md rounded-lg hover:bg-green-500 transition duration-300">
+                        <img src={home} alt="Dashboard Icon" className="w-10 h-10 mr-2 ml-2"/>
+                        <a href='#' className='text-3xl text-emerald-700 hover:text-white'>Dashboard</a>
+                    </li>
+                    <li className="flex items-center mt-4 py-4 shadow-md rounded-lg hover:bg-green-500 transition duration-300">
+                        <img src={controller} alt="Control Icon" className="w-10 h-10 mr-2 ml-2 " />
+                        <a href='#' className='text-3xl text-emerald-700 hover:text-white'>Control</a>
+                    </li>
+                    <li className="flex items-center mt-4 py-4 shadow-md rounded-lg hover:bg-green-500 transition duration-300">
+                        <img src={setting} alt="Setting Icon" className="w-10 h-10 mr-2 ml-2" />
+                        <a href='#' className='text-3xl text-emerald-700 hover:text-white'>Setting</a>
+                    </li>
+                </ul>
             </div>
         </div>
     </>
