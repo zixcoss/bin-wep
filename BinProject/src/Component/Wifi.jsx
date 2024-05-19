e7e65dd828715068c391720cda8cd6663638eb01
Nontawat Noitubtim <139257120+4mGroot@users.noreply.github.com>
2024-05-19 15:44:43 +0700

    ColorPageWifi

--- BinProject/src/Component/Wifi.jsx
+++ BinProject/src/Component/Wifi.jsx
@@ -11,11 +11,11 @@ function Wifi() {
           <form className="space-y-4">
             <div>
               <label className="block text-left text-sm font-medium text-gray-700 mb-2">SSID/Name wifi</label>
-              <input type="text" id="wifiName" className="form-input mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500" placeholder="Wi-Fi Name" />
+              <input type="text" id="wifiName" className="form-input mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-emerald-900" placeholder="Wi-Fi Name" />
             </div>
             <div>
               <label className="block text-left text-sm font-medium text-gray-700 mb-2">Wi-Fi Password</label>
-              <input type="password" id="wifiPassword" className="form-input mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500" placeholder="Wi-Fi Password" />
+              <input type="password" id="wifiPassword" className="form-input mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-emerald-900" placeholder="Wi-Fi Password" />
             </div>
             <button type="submit" className="w-full py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-emerald-900 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
             Connect
