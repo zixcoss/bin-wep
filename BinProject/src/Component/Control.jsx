cb2794505bee5db6677325be18ec2b19e173119a
Nontawat Noitubtim <139257120+4mGroot@users.noreply.github.com>
2024-06-23 00:08:42 +0700

    fixnavbarinpagecontrol

--- BinProject/src/Component/Control.jsx
+++ BinProject/src/Component/Control.jsx
@@ -1,37 +1,44 @@
-import React from 'react'
-import Sidebar from './Sidebar'
+import React from 'react';
+import Sidebar from './Sidebar';
+import Navbar from './Navbar';
+
 function Control() {
   return (
     <>
-        <div className='flex'>
-            <Sidebar/>
-            <div className="flex w-full justify-center items-center h-screen ">
-            <div className="relative flex items-center justify-center w-3/4 h-3/4 bg-green-200 border-4 border-green-600 rounded-full shadow-lg">
-              {/* ปุ่มขึ้น */}
-              <button className="absolute top-6 w-44 h-44 bg-emerald-900 text-white border-2 border-gray-500 rounded-full focus:outline-none hover:bg-gray-400">
-                ▲
-              </button>
-              {/* ปุ่มลง */}
-              <button className="absolute bottom-6 w-44 h-44 bg-emerald-900 text-white border-2 border-gray-500 rounded-full focus:outline-none hover:bg-gray-400">
-                ▼
-              </button>
-              {/* ปุ่มซ้าย */}
-              <button className="absolute left-6 w-44 h-44 bg-emerald-900 text-white border-2 border-gray-500 rounded-full focus:outline-none hover:bg-gray-400">
-                ◄
-              </button>
-              {/* ปุ่มขวา */}
-              <button className="absolute right-6 w-44 h-44 bg-emerald-900 text-white border-2 border-gray-500 rounded-full focus:outline-none hover:bg-gray-400">
-                ►
-              </button>
-              {/* กล่องครอบจอย */}
-              <div className="flex items-center justify-center w-32 h-32 bg-white border-4 border-blue-600 rounded-full">
-                <span className="text-2xl font-bold">Joy</span>
-              </div>
+      <div className='flex flex-col md:flex-row h-screen'>
+        <div className='hidden md:block'>
+          <Sidebar />
+        </div>
+        <div className="flex flex-col md:flex-row w-full justify-center items-center h-full p-4">
+          <div className='block md:hidden'>
+            <Navbar />
+          </div>
+          <div className="relative flex flex-col items-center justify-center w-full md:w-3/4 h-full md:h-3/4 bg-green-200 border-4 border-green-600 rounded-full shadow-lg">
+            {/* ปุ่มขึ้น */}
+            <button className="absolute top-4 md:top-6 w-20 h-20 md:w-32 md:h-32 lg:w-44 lg:h-44 bg-emerald-900 text-white border-2 border-gray-500 rounded-full focus:outline-none hover:bg-gray-400">
+              ▲
+            </button>
+            {/* ปุ่มลง */}
+            <button className="absolute bottom-4 md:bottom-6 w-20 h-20 md:w-32 md:h-32 lg:w-44 lg:h-44 bg-emerald-900 text-white border-2 border-gray-500 rounded-full focus:outline-none hover:bg-gray-400">
+              ▼
+            </button>
+            {/* ปุ่มซ้าย */}
+            <button className="absolute left-4 md:left-6 w-20 h-20 md:w-32 md:h-32 lg:w-44 lg:h-44 bg-emerald-900 text-white border-2 border-gray-500 rounded-full focus:outline-none hover:bg-gray-400">
+              ◄
+            </button>
+            {/* ปุ่มขวา */}
+            <button className="absolute right-4 md:right-6 w-20 h-20 md:w-32 md:h-32 lg:w-44 lg:h-44 bg-emerald-900 text-white border-2 border-gray-500 rounded-full focus:outline-none hover:bg-gray-400">
+              ►
+            </button>
+            {/* กล่องครอบจอย */}
+            <div className="flex items-center justify-center w-24 h-24 md:w-32 md:h-32 lg:w-32 lg:h-32 bg-white border-4 border-blue-600 rounded-full">
+              <span className="text-2xl font-bold">Joy</span>
             </div>
           </div>
         </div>
+      </div>
     </>
   )
 }
 
-export default Control
+export default Control;
--- BinProject/src/Component/Navbar.jsx
+++ BinProject/src/Component/Navbar.jsx
@@ -8,23 +8,24 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <div className="w-full bg-green-200 p-4 flex justify-between items-center shadow-xl rounded-lg">
-      <div className="flex items-center">
-        <div className="w-12 h-12 mr-4">
+    <div className="w-full bg-green-200 p-4 flex justify-between items-center shadow-xl rounded-lg ">
+      <div className="flex flex-col items-center mb-4 sm:mb-0 sm:flex-row w-full">
+        <div className="ml-4 w-10 h-10 sm:w-12 h-12 mr-4">
           <img src={Bin} alt="My Image" />
         </div>
-        <div className="text-3xl font-bold text-emerald-900">BIN</div>
+        <div className="text-3xl font-bold text-emerald-900 ">BIN</div>
       </div>
-      <nav className="flex space-x-4">
-        <Link to='/Dashboard' className="flex flex-col items-center hover:bg-green-500 transition duration-300 p-2 rounded-lg">
+      <div className="flex w-1 h-12 bg-green-400 mx-4 sm:hidden"></div> {/* Divider for large screens */}
+      <nav className="flex space-x-4 w-full">
+        <Link to='/Dashboard' className="flex flex-col items-center hover:bg-green-500 transition duration-300 p-2 rounded-lg w-20 sm:w-24">
           <img src={home} alt="Dashboard Icon" className="w-8 h-8" />
           <span className='text-emerald-700 text-sm'>Dashboard</span>
         </Link>
-        <Link to='/Control' className="flex flex-col items-center hover:bg-green-500 transition duration-300 p-2 rounded-lg">
+        <Link to='/Control' className="flex flex-col items-center hover:bg-green-500 transition duration-300 p-2 rounded-lg w-20 sm:w-24">
           <img src={controller} alt="Control Icon" className="w-8 h-8" />
           <span className='text-emerald-700 text-sm'>Control</span>
         </Link>
-        <Link to='/Setting' className="flex flex-col items-center hover:bg-green-500 transition duration-300 p-2 rounded-lg">
+        <Link to='/Setting' className="flex flex-col items-center hover:bg-green-500 transition duration-300 p-2 rounded-lg w-20 sm:w-24">
           <img src={setting} alt="Setting Icon" className="w-8 h-8" />
           <span className='text-emerald-700 text-sm'>Setting</span>
         </Link>
