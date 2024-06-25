dfcaac9af8282fd673bdd24095be4c1cb4e60ccb
Nontawat Noitubtim <139257120+4mGroot@users.noreply.github.com>
2024-06-25 23:55:51 +0700

    dasboad_844*390

--- BinProject/tailwind.config.js
+++ BinProject/tailwind.config.js
@@ -5,7 +5,16 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'portrait-mobile': {'raw': '(max-width: 390px) and (max-height: 844px) and (orientation: portrait)'},
+        'landscape-mobile': {'raw': '(max-width: 844px) and (max-height: 390px) and (orientation: landscape)'},
+        'portrait-tablet': {'raw': '(max-width: 768px) and (max-height: 1024px) and (orientation: portrait)'},
+        'landscape-tablet': {'raw': '(max-width: 1024px) and (max-height: 768px) and (orientation: landscape)'},
+        'portrait-desktop': {'raw': '(max-width: 1024px) and (max-height: 1366px) and (orientation: portrait)'},
+        'landscape-desktop': {'raw': '(max-width: 1366px) and (max-height: 1024px) and (orientation: landscape)'},
+      },
+    },
   },
   plugins: [],
 }
